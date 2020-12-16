68ee7bcadb0acfac6c2a67c8f93d6693ecbef9ee
skr19930617 <llawliet_162@yahoo.co.jp>
2020-12-16 16:08:15 +0900

    fix code style

--- backend/lib/irrp.py
+++ backend/lib/irrp.py
@@ -1,9 +1,16 @@
+"""
+ir recording and playbacking tool
+"""
 import time
+from typing import List, Dict, Union, Any
 
 import pigpio
 
 
 class Irrp:
+    """
+    record and playback class
+    """
     GLITCH = 100  # ignore edges shorter than glitch microseconds, default 100 us
     PRE_MS = 200  # expect post milliseconds of silence after code, default 15 ms
     POST_MS = 15  # expect pre milliseconds of silence before code, default 200 ms
@@ -21,136 +28,194 @@ class Irrp:
     RECORD_PIN = 18
     PLAYBACK_PIN = 17
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._last_tick = 0
         self._in_code = False
-        self._press1 = []
-        self._press2 = []
-        self._code = []
+        self._press1: List[float] = []
+        self._press2: List[float] = []
+        self._code: List[float] = []
         self._fetching_code = False
 
-    def _carrier(self, gpio, frequency, micros):
-        wf = []
+    @staticmethod
+    def _carrier(
+            gpio: int,
+            frequency: float,
+            micros: float) -> List[float]:
+        """
+        Generate carrier square wave.
+        """
+        waveform = []
         cycle = 1000.0 / frequency
         cycles = int(round(micros / cycle))
-        on = int(round(cycle / 2.0))
+        on_pulse = int(round(cycle / 2.0))
         sofar = 0
-        for c in range(cycles):
-            target = int(round((c + 1) * cycle))
-            sofar += on
-            off = target - sofar
-            sofar += off
-            wf.append(pigpio.pulse(1 << gpio, 0, on))
-            wf.append(pigpio.pulse(0, 1 << gpio, off))
-        return wf
+        for cycle in range(cycles):
+            target = int(round((cycle + 1) * cycle))
+            sofar += on_pulse
+            off_pulse = target - sofar
+            sofar += off_pulse
+            waveform.append(pigpio.pulse(1 << gpio, 0, on_pulse))
+            waveform.append(pigpio.pulse(0, 1 << gpio, off_pulse))
+        return waveform
 
-    def _normalise(self, c):
-        entries = len(c)
-        p = [0] * entries  # Set all entries not processed.
-        for i in range(entries):
-            if not p[i]:  # Not processed?
-                v = c[i]
-                tot = v
+    def _normalise(self) -> None:
+        """
+        Typically a code will be made up of two or three distinct
+        marks (carrier) and spaces (no carrier) of different lengths.
+
+        Because of transmission and reception errors those pulses
+        which should all be x micros long will have a variance around x.
+
+        This function identifies the distinct pulses and takes the
+        average of the lengths making up each distinct pulse.  Marks
+        and spaces are processed separately.
+
+        This makes the eventual generation of waves much more efficient.
+
+        Input
+
+            M    S   M   S   M   S   M    S   M    S   M
+        9000 4500 600 540 620 560 590 1660 620 1690 615
+
+        Distinct marks
+
+        9000                average 9000
+        600 620 590 620 615 average  609
+
+        Distinct spaces
+
+        4500                average 4500
+        540 560             average  550
+        1660 1690           average 1675
+
+        Output
+
+            M    S   M   S   M   S   M    S   M    S   M
+        9000 4500 609 550 609 550 609 1675 609 1675 609
+        """
+        entries = len(self._code)
+        pulse = [0] * entries
+        for index, value in enumerate(self._code):
+            if not pulse[index]:  # Not processed?
+                tot = value
                 similar = 1.0
 
                 # Find all pulses with similar lengths to the start pulse.
-                for j in range(i + 2, entries, 2):
-                    if not p[j]:  # Unprocessed.
-                        if (c[j] *
-                            self.TOLER_MIN) < v < (c[j] *
-                                                   self.TOLER_MAX):  # Similar.
-                            tot = tot + c[j]
+                for j in range(index + 2, entries, 2):
+                    if not pulse[j]:  # Unprocessed.
+                        if (self._code[j] *
+                                self.TOLER_MIN) < value < (self._code[j] *
+                                                           self.TOLER_MAX):  # Similar.
+                            tot = tot + self._code[j]
                             similar += 1.0
 
                 # Calculate the average pulse length.
                 newv = round(tot / similar, 2)
-                c[i] = newv
+                self._code[index] = newv
 
                 # Set all similar pulses to the average value.
-                for j in range(i + 2, entries, 2):
-                    if not p[j]:  # Unprocessed.
-                        if (c[j] *
-                            self.TOLER_MIN) < v < (c[j] *
-                                                   self.TOLER_MAX):  # Similar.
-                            c[j] = newv
-                            p[j] = 1
+                for j in range(index + 2, entries, 2):
+                    if not pulse[j]:  # Unprocessed.
+                        if (self._code[j] *
+                                self.TOLER_MIN) < value < (self._code[j] *
+                                                           self.TOLER_MAX):  # Similar.
+                            self._code[j] = newv
+                            pulse[j] = 1
 
-    def _compare(self, p1, p2):
-        if len(p1) != len(p2):
+    def _compare(self, pulse1: List[float], pulse2: List[float]) -> bool:
+        """
+        Check that both recodings correspond in pulse length to within
+        TOLERANCE%.  If they do average the two recordings pulse lengths.
+
+        Input
+
+                M    S   M   S   M   S   M    S   M    S   M
+        1: 9000 4500 600 560 600 560 600 1700 600 1700 600
+        2: 9020 4570 590 550 590 550 590 1640 590 1640 590
+
+        Output
+
+        A: 9010 4535 595 555 595 555 595 1670 595 1670 595
+        """
+        if len(pulse1) != len(pulse2):
             return False
 
-        for i in range(len(p1)):
-            v = p1[i] / p2[i]
-            if (v < self.TOLER_MIN) or (v > self.TOLER_MAX):
+        for value1, value2 in zip(pulse1, pulse2):
+            value = value1 / value2
+            if (value < self.TOLER_MIN) or (value > self.TOLER_MAX):
                 return False
 
-        for i in range(len(p1)):
-            p1[i] = int(round((p1[i] + p2[i]) / 2.0))
+        for index, value in enumerate(pulse1):
+            pulse1[index] = int(round((pulse1[index] + pulse2[index]) / 2.0))
 
         return True
 
-    def _tidy_mark_space(self, recorded, base):
+    def _tidy_mark_space(self, recorded: List[float], base: int) -> None:
 
-        ms = {}
+        millisec: Dict[Any, Any] = {}
 
-        rl = len(recorded)
-        for i in range(base, rl, 2):
-            if recorded[i] in ms:
-                ms[recorded[i]] += 1
+        for i in range(base, len(recorded), 2):
+            if recorded[i] in millisec:
+                millisec[recorded[i]] += 1
             else:
-                ms[recorded[i]] = 1
+                millisec[recorded[i]] = 1
 
-        v = None
+        value = None
 
-        for plen in sorted(ms):
-            if v is None:
-                e = [plen]
-                v = plen
-                tot = plen * ms[plen]
-                similar = ms[plen]
+        for plen in sorted(millisec):
+            if value is None:
+                tmp = [plen]
+                value = plen
+                tot = plen * millisec[plen]
+                similar = millisec[plen]
 
-            elif plen < (v * self.TOLER_MAX):
-                e.append(plen)
-                tot += (plen * ms[plen])
-                similar += ms[plen]
+            elif plen < (value * self.TOLER_MAX):
+                tmp.append(plen)
+                tot += (plen * millisec[plen])
+                similar += millisec[plen]
 
             else:
-                v = int(round(tot / float(similar)))
-                for i in e:
-                    ms[i] = v
-                e = [plen]
-                v = plen
-                tot = plen * ms[plen]
-                similar = ms[plen]
+                value = int(round(tot / float(similar)))
+                for i in tmp:
+                    millisec[i] = value
+                tmp = [plen]
+                value = plen
+                tot = plen * millisec[plen]
+                similar = millisec[plen]
 
-        v = int(round(tot / float(similar)))
-        for i in e:
-            ms[i] = v
+        value = int(round(tot / float(similar)))
+        for i in tmp:
+            millisec[i] = value
 
-        rl = len(recorded)
-        for i in range(base, rl, 2):
-            recorded[i] = ms[recorded[i]]
+        for i in range(base, len(recorded), 2):
+            recorded[i] = millisec[recorded[i]]
 
-    def _tidy(self, recorded):
+    def _tidy(self, recorded: List[float]) -> None:
         self._tidy_mark_space(recorded, 0)
         self._tidy_mark_space(recorded, 1)
 
-    def _end_of_code(self):
+    def _end_of_code(self) -> None:
+        """
+        finish fetching code if code is long enough
+        """
         if len(self._code) > self.SHORT:
-            self._normalise(self._code)
+            self._normalise()
             self._fetching_code = False
         else:
             self._code = []
             print("Short code, probably a repeat, try again")
 
-    def _record(self):
-        pi = pigpio.pi()
-        if not pi.connected:
+    def _record(self) -> List[float]:
+        """
+        record ir pulse
+        """
+        pigpio_pi = pigpio.pi()
+        if not pigpio_pi.connected:
             raise Exception("cannot connect to pigpio")
-        pi.set_mode(self.RECORD_PIN, pigpio.INPUT)
-        pi.set_glitch_filter(self.RECORD_PIN, self.GLITCH)
+        pigpio_pi.set_mode(self.RECORD_PIN, pigpio.INPUT)
+        pigpio_pi.set_glitch_filter(self.RECORD_PIN, self.GLITCH)
 
-        def cbf(gpio, level, tick):
+        def cbf(gpio: int, level: int, tick: int) -> None:
             if level != pigpio.TIMEOUT:
                 edge = pigpio.tickDiff(self._last_tick, tick)
                 self._last_tick = tick
@@ -161,18 +226,18 @@ class Irrp:
                             not self._in_code):  # Start of a code.
                         self._in_code = True
                         # Start watchdog.
-                        pi.set_watchdog(gpio, self.POST_MS)
+                        pigpio_pi.set_watchdog(gpio, self.POST_MS)
 
                     # End of a code.
                     elif (edge > self.POST_US) and self._in_code:
                         self._in_code = False
-                        pi.set_watchdog(gpio, 0)  # Cancel watchdog.
+                        pigpio_pi.set_watchdog(gpio, 0)  # Cancel watchdog.
                         self._end_of_code()
 
                     elif self._in_code:
                         self._code.append(edge)
             else:
-                pi.set_watchdog(gpio, 0)  # Cancel watchdog.
+                pigpio_pi.set_watchdog(gpio, 0)  # Cancel watchdog.
                 if self._in_code:
                     self._in_code = False
                     self._end_of_code()
@@ -181,26 +246,35 @@ class Irrp:
         print("Press button")
         self._code = []
         self._fetching_code = True
-        pi.callback(self.RECORD_PIN, pigpio.EITHER_EDGE, cbf)
+        pigpio_pi.callback(self.RECORD_PIN, pigpio.EITHER_EDGE, cbf)
         while self._fetching_code:
             time.sleep(0.1)
         print("Okay")
         return self._code[:]
 
-    def record(self):
+    def record(self) -> List[float]:
+        """
+        record with no comfirm
+        """
         recorded = self._record()
         self._tidy(recorded)
         return recorded
 
-    def record_first(self):
+    def record_first(self) -> List[float]:
+        """
+        record first press
+        """
         self._press1 = self._record()
         return self._press1
 
-    def record_confirm(self):
+    def record_confirm(self) -> Union[bool, List[float]]:
+        """
+        record second press and compare to the first press
+        """
         if not self._press1:
             raise Exception("first press not recorded")
 
-        recorded = []
+        recorded: List[float] = []
         for try_count in range(3):
             self._press2 = self._record()
             the_same = self._compare(self._press1, self._press2)
@@ -208,12 +282,11 @@ class Irrp:
                 recorded = self._press1[:]
                 print("Okay")
                 break
+            if try_count <= 3:
+                print("No match")
             else:
-                if try_count <= 3:
-                    print("No match")
-                else:
-                    print("Giving up")
-                time.sleep(0.5)
+                print("Giving up")
+            time.sleep(0.5)
 
         self._press1 = []
         self._press2 = []
@@ -222,48 +295,51 @@ class Irrp:
         self._tidy(recorded)
         return recorded
 
-    def playback(self, code):
-        pi = pigpio.pi()
-        pi.set_mode(self.PLAYBACK_PIN, pigpio.OUTPUT)
-        pi.wave_add_new()
+    def playback(self, code: List[int]) -> None:
+        """
+        play back the given code
+        """
+        pigpio_pi = pigpio.pi('soft', 8888)
+        pigpio_pi.set_mode(self.PLAYBACK_PIN, pigpio.OUTPUT)
+        pigpio_pi.wave_add_new()
         emit_time = time.time()
 
-        marks_wid = {}
-        spaces_wid = {}
-        wave = [0] * len(code)
+        marks_wid: Dict[int, float] = {}
+        spaces_wid: Dict[int, float] = {}
+        wave = [0.0] * len(code)
 
-        for i in range(0, len(code)):
-            ci = code[i]
-            if i & 1:  # Space
-                if ci not in spaces_wid:
-                    pi.wave_add_generic([pigpio.pulse(0, 0, ci)])
-                    spaces_wid[ci] = pi.wave_create()
-                wave[i] = spaces_wid[ci]
+        for index, value in enumerate(code):
+            if index & 1:  # Space
+                if value not in spaces_wid:
+                    pigpio_pi.wave_add_generic([pigpio.pulse(0, 0, value)])
+                    spaces_wid[value] = pigpio_pi.wave_create()
+                wave[index] = spaces_wid[value]
             else:  # Mark
-                if ci not in marks_wid:
-                    wf = self._carrier(self.PLAYBACK_PIN, self.FREQ, ci)
-                    pi.wave_add_generic(wf)
-                    marks_wid[ci] = pi.wave_create()
-                wave[i] = marks_wid[ci]
+                if value not in marks_wid:
+                    waveform = self._carrier(
+                        self.PLAYBACK_PIN, self.FREQ, value)
+                    pigpio_pi.wave_add_generic(waveform)
+                    marks_wid[value] = pigpio_pi.wave_create()
+                wave[index] = marks_wid[value]
 
         delay = emit_time - time.time()
 
         if delay > 0.0:
             time.sleep(delay)
 
-        pi.wave_chain(wave)
+        pigpio_pi.wave_chain(wave)
 
-        while pi.wave_tx_busy():
+        while pigpio_pi.wave_tx_busy():
             time.sleep(0.002)
 
         emit_time = time.time() + self.GAP_S
 
         for i in marks_wid:
-            pi.wave_delete(marks_wid[i])
+            pigpio_pi.wave_delete(marks_wid[i])
         marks_wid = {}
 
         for i in spaces_wid:
-            pi.wave_delete(spaces_wid[i])
+            pigpio_pi.wave_delete(spaces_wid[i])
         spaces_wid = {}
 
-        pi.stop()
+        pigpio_pi.stop()
--- tests/code_style_check.sh
+++ tests/code_style_check.sh
@@ -2,7 +2,7 @@
 pylint backend/lib/irrp.py --rcfile ./.pylintrc
 
 # typing for documentation purpose
-mypy backend/lib/irrp.py --strict
+mypy backend/lib/irrp.py --strict --ignore-missing-imports
 
 # typing for test codes
 # mypy tests
